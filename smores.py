5f4ab400354135f0e0794f2006579f247c6a7f15
Jaz Walker <playallday723@gmail.com>
2019-10-22 15:23:01 -0500

    12 commit boii 3:22 october 22

--- smores.py
+++ smores.py
@@ -159,6 +159,20 @@ else:
         else:
             print("not bigger than much")
 
+def montePi(numDarts):
+
+    inCircle = 0
+
+    for i in range(numDarts):
+        x = random.random()
+        y = random.random()
+
+        distance = math.sqrt(x**2 + y**2)
+
+    pi = inCircle / numDarts * 4
+    return pi
+
+
 
 
 
